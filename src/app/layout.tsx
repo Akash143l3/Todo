6370661554dd3637d65712b3fb2b27e0b715555f
Akash143l3 <Team3Nala@gmail.com>
2024-09-21 08:07:01 +0530

    Added `publishableKey` prop to `ClerkProvider` component.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,10 +17,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
     </ClerkProvider>
   );
 }
